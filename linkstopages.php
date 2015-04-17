ab68e5017ef33853dfd93873b66faf71825edc13
praveen tissera <praveen.tissera@peakadventuretravel.com>
2015-04-17 08:02:46 +0530

    commit initial files to the repo

---
+++ linkstopages.php
@@ -0,0 +1,13 @@
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="/stylesheets/main.css" />
+	</head>
+  <body>
+    <ul>   
+		<li><a href="/googletutorials/helloworld.php">go to Hello World</a></li>
+		<li><a href="/googletutorials/forms.php">go to Form</a></li>
+		<li><a href="/googletutorials/linkstaticfile.php">go to Static File Link</a></li>
+		<li><a href="/googletutorials/userservice.php">go to User Service</a></li>
+	</ul>
+  </body>
+</html>
